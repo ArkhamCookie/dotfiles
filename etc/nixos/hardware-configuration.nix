fc72dd4f03525a76a82d1b99b7bcae0a4f1ab6fa
ArkhamCookie <git@mail.arkhamcookie.com>
2026-09-02 10:19:31 -0400

    feat: add resume device for suspending in the future

--- etc/nixos/hardware-configuration.nix
+++ etc/nixos/hardware-configuration.nix
@@ -29,6 +29,8 @@
 
   swapDevices = [{ device = "/dev/disk/by-label/SWAP"; }];
 
+  boot.resumeDevice = "/dev/disk/by-label/SWAP";
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
